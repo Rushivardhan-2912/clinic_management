cba459e9bb5d7f981cdfd7715bf93dccde1002f6
Rushivardhan-2912 <rushivardhan289@gmail.com>
2025-04-22 15:09:10 +0530

    Code Outline

---
+++ src/test/java/com/soprasteria/clinic/appointment/ClinicAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soprasteria.clinic.appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
